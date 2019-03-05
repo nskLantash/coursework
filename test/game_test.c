96f9ff8edbbd0fe5591390009e0d7ff4573b2a6c
Lantash <nsklantash@gmail.com>
2019-03-05 15:53:02 +0700

    Add first test

---
+++ test/game_test.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "ctest.h"
+#include "../src/game.h"
+#include "../src/terminal.h"
+#include "../src/design.h"
+
+void init_matrix(int **world)
+{
+	world[0][0] = 0; world[0][1] = 1; world[0][2] = 1; world[0][3] = 0; 
+	world[1][0] = 0; world[1][1] = 1; world[1][2] = 0; world[1][3] = 1; 
+	world[2][0] = 0; world[2][1] = 0; world[2][2] = 0; world[2][3] = 0; 
+	world[3][0] = 1; world[3][1] = 0; world[3][2] = 1; world[3][3] = 0; 
+}
+
+CTEST(suite1, test1) {
+	int **matrix;
+	matrix = (int**)malloc(4 * sizeof(int*));
+    for (int i = 0; i < 4; i++) {  // цикл по строкам 
+    	// Выделение памяти под хранение строк
+    	matrix[i] = (int*)malloc(4 * sizeof(int));
+	}
+	init_matrix(matrix);
+
+	unsigned int real = get_live_count(matrix, 4, 4);
+	unsigned int expected = 6;
+	ASSERT_EQUAL(real, expected);
+}
+
